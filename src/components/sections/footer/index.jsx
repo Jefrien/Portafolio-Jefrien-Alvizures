6ec86f98e2a1ed184e936a33fb924181724c7130
Jefrien Alvizures <jefrienalvizures@gmail.com>
2025-07-11 22:01:13 -0600

    feat: add footer section with developer credit and styling

--- src/components/sections/footer/index.jsx
+++ src/components/sections/footer/index.jsx
@@ -1,7 +1,7 @@
 
 export const FooterSection = () => {
   return (
-    <div className="text-center text-white py-4 bg-neutral-800 dark:bg-accent">
+    <div className="text-center text-white py-4 bg-neutral-800 dark:bg-accent text-xs md:text-sm font-semibold">
         Developed with ❤️ by Jefrien Alvizures
     </div>
   )
